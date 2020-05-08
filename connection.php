eca503a7d22af7dcf1a795da7b09890258e0accd
Trey Roche <proche@luc.edu>
2020-05-07 23:10:21 -0500

    rewrite of connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
 error_reporting(1);
-$con=mysqli_connect('us-cdbr-east-06.cleardb./net', 'bb617abde046ff', 'c8a85971', 'heroku_44bf3c29f218952') or die("Could not connect"); #update for local configuration
+$con=new mysqli('us-cdbr-east-06.cleardb./net', 'bb617abde046ff', 'c8a85971', 'heroku_44bf3c29f218952') or die("Could not connect"); #update for local configuration
 ?>
 
